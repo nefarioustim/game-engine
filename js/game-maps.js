4bfff83574f1620f78601c38821a2d974ee00b5b
Tim Huegdon <tim@nefariousdesigns.co.uk>
2012-05-16 15:01:35 +0100

    Add basic Map class and a game-maps file.

---
+++ js/game-maps.js
@@ -0,0 +1,9 @@
+(function(GENG){
+    GENG.getGameMaps = function(ctx) {
+        var lvl1Tiles = new Image();
+
+        lvl1Tiles.src = "img/lvl1.gif";
+    };
+
+    window.GENG = GENG;
+})(window.GENG || {});
---
+++ js/map.js
@@ -0,0 +1,13 @@
+(function(GENG){
+    GENG.Map = function() {
+        return this;
+    };
+
+    GENG.Map.prototype = {
+        getTiles: function(tileImg) {
+
+        }
+    };
+
+    window.GENG = GENG;
+})(window.GENG || {});
