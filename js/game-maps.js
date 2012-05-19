6ef119ca4f7fef5b9ba4ab096fa326931afed6e4
Tim Huegdon <tim@nefariousdesigns.co.uk>
2012-05-19 19:52:28 +0100

    Load in the lvl1 tile image and pass it to a new Map instance.

--- js/game-maps.js
+++ js/game-maps.js
@@ -1,8 +1,12 @@
 (function(GENG){
     GENG.getGameMaps = function(ctx) {
-        var lvl1Tiles = new Image();
+        var lvl1Tiles = new Image(),
+            maps = {};
 
         lvl1Tiles.src = "img/lvl1.gif";
+
+        maps.level1 = new GENG.Map(ctx);
+        maps.level1.loadTiles(lvl1Tiles);
     };
 
     window.GENG = GENG;
